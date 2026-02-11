c952013f399b5697794a4bd8cb0fbf3e5c66b6e6
Stephen Finucane <stephen@that.guru>
2026-02-11 12:52:44 +0000

    Add packaging, test harness
    Minimal versions of the configuration from setuptools v81.0.0.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

---
+++ README.rst
@@ -0,0 +1,4 @@
+``standard-pkg_resources``
+==========================
+
+Redistribution of the pkg_resources library provided by setuptools.
---
+++ conftest.py
@@ -0,0 +1,63 @@
+import platform
+
+import pytest
+
+pytest_plugins = 'setuptools.tests.fixtures'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--package_name",
+        action="append",
+        default=[],
+        help="list of package_name to pass to test functions",
+    )
+    parser.addoption(
+        "--integration",
+        action="store_true",
+        default=False,
+        help="run integration tests (only)",
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "integration: integration tests")
+    config.addinivalue_line("markers", "uses_network: tests may try to download files")
+    _IntegrationTestSpeedups.disable_plugins_already_run(config)
+
+
+collect_ignore = [
+    'pkg_resources/tests/data',
+]
+
+
+@pytest.fixture(autouse=True)
+def _skip_integration(request):
+    _IntegrationTestSpeedups.conditional_skip(request)
+
+
+class _IntegrationTestSpeedups:
+    """Speed-up integration tests by only running what does not run in other tests."""
+
+    RUNS_ON_NORMAL_TESTS = ("checkdocks", "cov", "mypy", "perf", "ruff")
+
+    @classmethod
+    def disable_plugins_already_run(cls, config):
+        if config.getoption("--integration"):
+            for plugin in cls.RUNS_ON_NORMAL_TESTS:  # no need to run again
+                config.pluginmanager.set_blocked(plugin)
+
+    @staticmethod
+    def conditional_skip(request):
+        running_integration_tests = request.config.getoption("--integration")
+        is_integration_test = request.node.get_closest_marker("integration")
+        if running_integration_tests and not is_integration_test:
+            pytest.skip("running integration tests only")
+        if not running_integration_tests and is_integration_test:
+            pytest.skip("skipping integration tests")
+
+
+@pytest.fixture
+def windows_only():
+    if platform.system() != 'Windows':
+        pytest.skip("Windows only")
